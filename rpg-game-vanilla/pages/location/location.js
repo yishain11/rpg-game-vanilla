631cb08406d193d906b45400a2e12fce3eb188b5
yishai-n-11 <yishain11@gmail.com>
2023-01-27 10:41:31 +0200

    added lnk to combat page

--- rpg-game-vanilla/pages/location/location.js
+++ rpg-game-vanilla/pages/location/location.js
@@ -4,7 +4,9 @@ import { genHeader } from "../../components/header.mjs";
 const url = new URL(window.location.href);
 const searchParams = new URLSearchParams(url.search);
 const locationName = searchParams.get('location');
+const h1 = document.getElementsByTagName("h1")[0];
 const title = document.getElementsByTagName('title')[0];
+h1.innerText = locationName;
 title.innerText = locationName;
 const main = document.getElementsByTagName('main')[0];
 const header = document.getElementsByTagName('header')[0
@@ -18,20 +20,44 @@ contracts.contracts.forEach(contract => {
 });
 
 function createContractCard(contract) {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    const title = document.createElement('h1');
-    title.innerText = 'wanted! preferably dead';
-    const subTitle = document.createElement('h3');
-    subTitle.innerText = contract.monster.name;
+    const card = genCard(contract);
+    const [title, subTitle] = genTitles(contract);
+    const img = styleImg();
+    const [p1, p2] = styleP(contract);
+    card.append(title, subTitle, img, p1, p2);
+    main.append(card);
+}
+
+function styleImg() {
     const img = document.createElement('img');
     img.src = '/assets/monsters/monster1.jpg';
     img.setAttribute('width', '90%');
     img.setAttribute('height', '90%');
+    return img;
+}
+
+function styleP(contract) {
     const p1 = document.createElement('p');
     const p2 = document.createElement('p');
     p1.innerText = `Gold Reward: ${contract.gold}`;
     p2.innerText = `XP Reward: ${contract.xp}`;
-    card.append(title, subTitle, img, p1, p2);
-    main.append(card);
+    return [p1, p2];
+}
+
+function genTitles(contract) {
+    const title = document.createElement('h1');
+    title.innerText = 'wanted! preferably dead';
+    const subTitle = document.createElement('h3');
+    subTitle.innerText = contract.monster.name;
+    return [title, subTitle];
+}
+
+function genCard(contract) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.addEventListener('click', () => {
+        localStorage.setItem('currentMonster', JSON.stringify(contract.monster));
+        window.location = '/pages/combat.html';
+    });
+    return card;
 }
